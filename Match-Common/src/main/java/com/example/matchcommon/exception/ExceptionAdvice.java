44f77c405b3fb97978a93cb5d8efd4229d1b6154
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-07-12 18:39:53 +0900

    :zap: : @Valid 전역 예외처리 추가

--- Match-Common/src/main/java/com/example/matchcommon/exception/ExceptionAdvice.java
+++ Match-Common/src/main/java/com/example/matchcommon/exception/ExceptionAdvice.java
@@ -1,32 +1,22 @@
 package com.example.matchcommon.exception;
 
 import com.example.matchcommon.reponse.CommonResponse;
-import com.example.matchcommon.reponse.ErrorReason;
-import com.example.matchcommon.reponse.ErrorResponse;
-import com.fasterxml.jackson.databind.ser.Serializers;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -60,43 +50,6 @@ public class ExceptionAdvice{
         return new ResponseEntity<>(CommonResponse.onFailure("REQUEST_ERROR", "요청 형식 에러", errors), null, HttpStatus.BAD_REQUEST);
     }
 
-    /*
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity ConstraintViolationExceptionHandler(
-            ConstraintViolationException e, HttpServletRequest request) {
-        System.out.println("에러");
-        Map<String, Object> bindingErrors = new HashMap<>();
-        e.getConstraintViolations()
-                .forEach(
-                        constraintViolation -> {
-                            List<String> propertyPath =
-                                    List.of(
-                                            constraintViolation
-                                                    .getPropertyPath()
-                                                    .toString()
-                                                    .split("\\."));
-                            String path =
-                                    propertyPath.stream()
-                                            .skip(propertyPath.size() - 1L)
-                                            .findFirst()
-                                            .orElse(null);
-                            bindingErrors.put(path, constraintViolation.getMessage());
-                        });
-
-        ErrorReason errorReason =
-                ErrorReason.builder()
-                        .code("BAD_REQUEST")
-                        .status(400)
-                        .reason(bindingErrors.toString())
-                        .build();
-        System.out.println(bindingErrors);
-        ErrorResponse errorResponse =
-                new ErrorResponse(errorReason, request.getRequestURL().toString());
-        System.out.println(errorResponse.getReason());
-        return new ResponseEntity<>(CommonResponse.onFailure("REQUSET_ERROR", "요청 에러", bindingErrors), null, HttpStatus.BAD_REQUEST);
-
-    }
-    */
 
 
     private void getExceptionStackTrace(Exception e, @AuthenticationPrincipal User user,
