ec67939a4b0b82a1d501fbf4b56cdd8cc72e9123
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-07-31 18:19:12 +0900

    :recycle: : NicePayments 예외코드 제공 X -> 0000 코드 아닐시 예외처리 추가

--- Match-Common/src/main/java/com/example/matchcommon/exception/BaseException.java
+++ Match-Common/src/main/java/com/example/matchcommon/exception/BaseException.java
@@ -4,12 +4,32 @@ import com.example.matchcommon.dto.ErrorReason;
 import com.example.matchcommon.exception.errorcode.BaseErrorCode;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 
 @Getter
 @AllArgsConstructor
 public class BaseException extends RuntimeException {
     private BaseErrorCode errorCode;
+
+    public BaseException(HttpStatus httpStatus, boolean b, String resultCode, String resultMsg) {
+        this.errorCode = new BaseErrorCode() {
+            @Override
+            public ErrorReason getErrorReason() {
+                return ErrorReason.builder().message(resultMsg).code(resultCode).isSuccess(b).build();
+            }
+
+            @Override
+            public String getExplainError() throws NoSuchFieldException {
+                return null;
+            }
+
+            @Override
+            public ErrorReason getErrorReasonHttpStatus() {
+                return ErrorReason.builder().message(resultMsg).code(resultCode).isSuccess(b).httpStatus(httpStatus).build();
+            }
+        };
+    }
 
     public ErrorReason getErrorReason() {
         return this.errorCode.getErrorReason();
