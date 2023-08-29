c11cefc79d288c974b6c68b77b651d00f25a74ba
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-08-29 15:55:45 +0900

    :zap: : Batch Health Check 추가

--- Match-Batch/src/main/java/com/example/matchbatch/MatchBatchApplication.java
+++ Match-Batch/src/main/java/com/example/matchbatch/MatchBatchApplication.java
@@ -4,13 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @ComponentScan(basePackages = {"com.example.matchbatch","com.example.matchdomain","com.example.matchinfrastructure", "com.example.matchcommon"})
 @EnableScheduling
 @EnableBatchProcessing
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @Slf4j
 public class MatchBatchApplication{
     public static void main(String[] args) {
