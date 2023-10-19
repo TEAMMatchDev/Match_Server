af619147d1512dc3049bd5f5b8a67e79ee5c0e22
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-10-19 11:33:36 +0900

    :recycle: : banner dto 직렬화 적용

--- Match-Api/src/main/java/com/example/matchapi/banner/dto/BannerRes.java
+++ Match-Api/src/main/java/com/example/matchapi/banner/dto/BannerRes.java
@@ -3,13 +3,15 @@ package com.example.matchapi.banner.dto;
 import com.example.matchdomain.banner.enums.BannerType;
 import lombok.*;
 
+import java.io.Serializable;
+
 public class BannerRes {
     @Getter
     @Setter
     @Builder
     @AllArgsConstructor
     @NoArgsConstructor
-    public static class BannerList {
+    public static class BannerList implements Serializable {
         private Long bannerId;
 
         private BannerType bannerType;
