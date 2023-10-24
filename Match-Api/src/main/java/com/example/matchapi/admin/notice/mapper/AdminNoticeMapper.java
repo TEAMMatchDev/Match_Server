a721b8f98edbd0a9ccc6f243ccc239e19e79a808
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-10-24 14:43:35 +0900

    :refactor: : AdminNotice 구현 (#104)

---
+++ Match-Api/src/main/java/com/example/matchapi/admin/notice/mapper/AdminNoticeMapper.java
@@ -0,0 +1,19 @@
+package com.example.matchapi.admin.notice.mapper;
+
+import com.example.matchapi.admin.notice.dto.NoticeUploadReq;
+import com.example.matchapi.common.model.ContentsList;
+import com.example.matchdomain.notice.entity.Notice;
+import com.example.matchdomain.notice.entity.NoticeContent;
+import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper
+public interface AdminNoticeMapper {
+    AdminNoticeMapper INSTANCE = Mappers.getMapper(AdminNoticeMapper.class);
+
+    List<NoticeContent> toEntityNoticeContents(List<ContentsList> contentsList);
+
+    Notice toEntityNotice(NoticeUploadReq noticeUploadReq);
+}
