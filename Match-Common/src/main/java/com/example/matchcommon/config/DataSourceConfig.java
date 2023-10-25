2c73a97bcf1688e1c7bb9359f99390ae4f9c52a6
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-10-25 11:44:07 +0900

    :zap: : master-slave 구조 config 작성 (#122)

---
+++ Match-Common/src/main/java/com/example/matchcommon/config/DataSourceConfig.java
@@ -0,0 +1,62 @@
+package com.example.matchcommon.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@Slf4j
+public class DataSourceConfig {
+    public static final String MASTER_DATASOURCE = "masterDataSource";
+    public static final String SLAVE_DATASOURCE = "slaveDataSource";
+
+    @Bean(MASTER_DATASOURCE)
+    @ConfigurationProperties(prefix = "spring.datasource.master.hikari")
+    public DataSource masterDataSource() {
+        log.info("master db active");
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean(SLAVE_DATASOURCE)
+    @ConfigurationProperties(prefix = "spring.datasource.slave.hikari")
+    public DataSource slaveDataSource() {
+        log.info("slave db active");
+        return DataSourceBuilder.create()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
+    @Bean
+    @DependsOn({MASTER_DATASOURCE, SLAVE_DATASOURCE})
+    public DataSource routingDataSource(
+            @Qualifier(MASTER_DATASOURCE) DataSource masterDataSource,
+            @Qualifier(SLAVE_DATASOURCE) DataSource slaveDataSource) {
+        RoutingDataSource routingDataSource = new RoutingDataSource();
+        Map<Object, Object> datasource = new HashMap<>();
+        datasource.put("master", masterDataSource);
+        datasource.put("slave", slaveDataSource);
+        routingDataSource.setTargetDataSources(datasource);
+        routingDataSource.setDefaultTargetDataSource(masterDataSource);
+        return routingDataSource;
+    }
+
+    @Primary
+    @Bean
+    @DependsOn("routingDataSource")
+    public LazyConnectionDataSourceProxy dataSource(DataSource routingDataSource){
+        return new LazyConnectionDataSourceProxy(routingDataSource);
+    }
+}
