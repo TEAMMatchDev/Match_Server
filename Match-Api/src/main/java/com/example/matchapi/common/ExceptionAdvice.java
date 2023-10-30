6ca241cbd2408e36bed86ffbbf5ec1c1defececf
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-10-30 14:11:41 +0900

    :recycle: : 에러 로깅 처리 Discord 로깅 비동기 처리(#104)

--- Match-Api/src/main/java/com/example/matchapi/common/ExceptionAdvice.java
+++ Match-Api/src/main/java/com/example/matchapi/common/ExceptionAdvice.java
@@ -3,14 +3,13 @@ package com.example.matchapi.common;
 import com.example.matchcommon.exception.BaseDynamicException;
 import com.example.matchcommon.exception.BaseException;
 import com.example.matchcommon.reponse.CommonResponse;
-import com.example.matchinfrastructure.discord.client.DiscordFeignClient;
-import com.example.matchinfrastructure.discord.convertor.DiscordConvertor;
+import com.example.matchdomain.user.entity.User;
+import com.example.matchinfrastructure.discord.service.DiscordService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -32,31 +31,31 @@ import java.util.stream.StreamSupport;
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class ExceptionAdvice {
-    private final DiscordFeignClient discordFeignClient;
-    private final DiscordConvertor discordConvertor;
+    private final DiscordService discordService;
 
 
 
 
     @ExceptionHandler(ConstraintViolationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity onConstraintValidationException(ConstraintViolationException e) {
-        Map<String, String> errors = e.getConstraintViolations().stream()
+    public ResponseEntity onConstraintValidationException(ConstraintViolationException exception, @AuthenticationPrincipal User user, HttpServletRequest request) {
+        Map<String, String> errors = exception.getConstraintViolations().stream()
                 .collect(Collectors.toMap(
                         violation -> StreamSupport.stream(violation.getPropertyPath().spliterator(), false)
                                 .reduce((first, second) -> second)
                                 .get().toString(),
                         ConstraintViolation::getMessage
                 ));
+        getExceptionStackTrace(exception, user, request, errors);
         return new ResponseEntity<>(CommonResponse.onFailure("REQUEST_ERROR", "요청 형식 에러 result 확인해주세요", errors), null, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity onMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ResponseEntity onMethodArgumentNotValidException(MethodArgumentNotValidException exception, @AuthenticationPrincipal User user, HttpServletRequest request) {
         Map<String, String> errors = new LinkedHashMap<>();
 
-        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
 
             String fieldName = fieldError.getField();
             String errorMessage = Optional.ofNullable(fieldError.getDefaultMessage()).orElse("");
@@ -68,31 +67,16 @@ public class ExceptionAdvice {
             errors.put(fieldName, errorMessage);
         }
 
+        getExceptionStackTrace(exception, user, request, errors);
+
         return new ResponseEntity<>(CommonResponse.onFailure("REQUEST_ERROR", "요청 형식 에러 result 확인해주세요", errors), null, HttpStatus.BAD_REQUEST);
-    }
-
-
-
-    private void getExceptionStackTrace(Exception e, @AuthenticationPrincipal User user,
-                                        HttpServletRequest request) {
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-
-        pw.append("\n==========================!!!ERROR TRACE!!!==========================\n");
-        pw.append("uri: " + request.getRequestURI() + " " + request.getMethod() + "\n");
-        if (user != null) {
-            pw.append("uid: " + user.getUsername() + "\n");
-        }
-        pw.append(e.getMessage());
-        pw.append("\n=====================================================================");
-        log.error(sw.toString());
     }
 
 
     @ExceptionHandler(value = BaseException.class)
     public ResponseEntity onKnownException(BaseException baseException,
                                            @AuthenticationPrincipal User user, HttpServletRequest request) {
-        getExceptionStackTrace(baseException, user, request);
+        getExceptionStackTrace(baseException, user, request, null);
 
         return new ResponseEntity<>(CommonResponse.onFailure(baseException.getErrorReasonHttpStatus().getCode(), baseException.getErrorReasonHttpStatus().getMessage(), baseException.getErrorReasonHttpStatus().getResult()),
                 null, baseException.getErrorReasonHttpStatus().getHttpStatus());
@@ -101,24 +85,55 @@ public class ExceptionAdvice {
     @ExceptionHandler(value = BaseDynamicException.class)
     public ResponseEntity onKnownDynamicException(BaseDynamicException baseDynamicException, @AuthenticationPrincipal User user,
                                       HttpServletRequest request) {
-        getExceptionStackTrace(baseDynamicException, user, request);
+        getExceptionStackTrace(baseDynamicException, user, request, null);
         return new ResponseEntity<>(CommonResponse.onFailure(baseDynamicException.getStatus().getErrorReason().getCode(), baseDynamicException.getStatus().getErrorReason().getMessage(), baseDynamicException.getData()), null,
                 baseDynamicException.getStatus().getErrorReasonHttpStatus().getHttpStatus());
     }
 
 
 
-
-    /*
     @ExceptionHandler(value = Exception.class)
     public ResponseEntity onException(Exception exception, @AuthenticationPrincipal User user,
                                       HttpServletRequest request) {
-        getExceptionStackTrace(exception, user, request);
-        String returnMessage = discordFeignClient.errorMessage(discordConvertor.Message(user, exception, request));
+        getExceptionStackTrace(exception, user, request, null);
+        if(user==null){
+            discordService.sendUnKnownMessage("로그인 되지 않은 유저", exception, request);
+        }
+        else{
+            discordService.sendUnKnownMessage(user.getUsername(), exception, request);
+        }
+
         return new ResponseEntity<>(CommonResponse.onFailure("500", exception.getMessage(), null), null,
                 HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-     */
+    private void getExceptionStackTrace(Exception e, User user, HttpServletRequest request, Map<String, String> errors) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+
+        pw.append("\n==========================!!!ERROR TRACE!!!==========================\n");
+        if(user != null) {
+            pw.append("USER_ID : " + user.getId()).append("\n");
+        }
+        pw.append("REQUEST_URI : ").append(request.getRequestURI()).append(" ").append(request.getMethod()).append("\n");
+        if (e instanceof BaseException) {
+            pw.append("ERROR_CODE : ").append(((BaseException) e).getErrorReason().getCode()).append("\n");
+            pw.append("ERROR_MESSAGE : ").append(((BaseException) e).getErrorReason().getMessage()).append("\n");
+        } else if (e instanceof BaseDynamicException) {
+            pw.append("ERROR_CODE : ").append(((BaseDynamicException) e).getStatus().getErrorReason().getCode()).append("\n");
+            pw.append("ERROR_MESSAGE : ").append(((BaseDynamicException) e).getStatus().getErrorReason().getMessage()).append("\n");
+        } else if(e instanceof MethodArgumentNotValidException | e instanceof ConstraintViolationException){
+            pw.append("ERROR_CODE : ").append("REQUEST_ERROR").append("\n");
+            pw.append("ERROR_MESSAGE : ").append(errors.toString()).append("\n");
+        }
+        else {
+            pw.append("ERROR_MESSAGE : ").append(e.getMessage()).append("\n");
+            pw.append("ERROR_MESSAGE : ").append(e.getLocalizedMessage()).append("\n");
+        }
+        pw.append("=====================================================================");
+        log.error(sw.toString());
+    }
+
+
 
 }
