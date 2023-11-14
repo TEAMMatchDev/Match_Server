5568a49b6ce78a3f4edbb7241b04a1b372a2d6cc
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-11-14 13:05:10 +0900

    :recycle: : 의존성 주입 변경 (#160)

--- Match-Infrastructure/src/main/java/com/example/matchinfrastructure/pay/portone/service/PortOneService.java
+++ Match-Infrastructure/src/main/java/com/example/matchinfrastructure/pay/portone/service/PortOneService.java
@@ -4,25 +4,41 @@ import com.example.matchcommon.properties.PortOneProperties;
 import com.siot.IamportRestClient.IamportClient;
 import com.siot.IamportRestClient.exception.IamportResponseException;
 import com.siot.IamportRestClient.request.CancelData;
+import com.siot.IamportRestClient.response.IamportResponse;
+import com.siot.IamportRestClient.response.Payment;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 
 @Service
+@RequiredArgsConstructor
 public class PortOneService {
-    private final IamportClient iamportClient;
+    private IamportClient iamportClient;
     private final PortOneProperties portOneProperties;
 
-    public PortOneService(PortOneProperties portOneProperties) {
-        this.portOneProperties = portOneProperties;
+    @PostConstruct
+    private void init() {
         this.iamportClient = new IamportClient(portOneProperties.getKey(), portOneProperties.getSecret());
-
     }
 
 
     public void refundPayment(String impUid) {
         try {
-            iamportClient.cancelPaymentByImpUid(new CancelData(impUid, true));
+            IamportResponse<Payment> paymentIamportResponse  = iamportClient.cancelPaymentByImpUid(new CancelData(impUid, true));
+            System.out.println(paymentIamportResponse.getCode());
+            System.out.println(paymentIamportResponse.getResponse());
+        } catch (IamportResponseException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void refundPaymentOrderId(String orderId) {
+        try {
+            IamportResponse<Payment> paymentIamportResponse  = iamportClient.cancelPaymentByImpUid(new CancelData(orderId, false));
+            System.out.println(paymentIamportResponse.getCode());
+            System.out.println(paymentIamportResponse.getResponse());
         } catch (IamportResponseException | IOException e) {
             throw new RuntimeException(e);
         }
