4cbd77aa55d42a3410f94fe91f5b2e2283c819e6
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-12-05 10:46:01 +0900

    :recycle: : 코드 수정 (#209)

--- Match-Api/src/main/java/com/example/matchapi/config/MultipartJackson2HttpMessageConverter.java
+++ Match-Api/src/main/java/com/example/matchapi/config/MultipartJackson2HttpMessageConverter.java
@@ -1,20 +1,29 @@
 package com.example.matchapi.config;
 
+
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Component;
 
 import java.lang.reflect.Type;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
+@Slf4j
 public class MultipartJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+
     /**
-     * Converter for support http request with header Content-Type: multipart/form-data
+     * "Content-Type: multipart/form-data" 헤더를 지원하는 HTTP 요청 변환기
      */
     public MultipartJackson2HttpMessageConverter(ObjectMapper objectMapper) {
         super(objectMapper, MediaType.APPLICATION_OCTET_STREAM);
     }
+
 
     @Override
     public boolean canWrite(Class<?> clazz, MediaType mediaType) {
