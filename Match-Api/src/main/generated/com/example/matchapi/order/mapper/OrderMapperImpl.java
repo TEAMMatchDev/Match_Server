a15005aef31b83fc0c146dca0dbbdf57070a9bf9
LimHyunwoo <81962309+imenuuu@users.noreply.github.com>
2023-12-07 11:09:33 +0900

    .

--- Match-Api/src/main/generated/com/example/matchapi/order/mapper/OrderMapperImpl.java
+++ Match-Api/src/main/generated/com/example/matchapi/order/mapper/OrderMapperImpl.java
@@ -9,7 +9,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-12-05T09:50:48+0900",
+    date = "2023-12-07T09:51:10+0900",
     comments = "version: 1.5.3.Final, compiler: javac, environment: Java 11.0.19 (Oracle Corporation)"
 )
 public class OrderMapperImpl implements OrderMapper {
